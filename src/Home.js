ca92d945f91b5507a6d034983ff49428449695e2
Kavya Halani <kavyahalani@gmail.com>
2018-11-30 23:52:24 +0530

    initial commit

---
+++ src/Home.js
@@ -0,0 +1,91 @@
+import React, {Component} from 'react';
+import Book from './Book'
+import { Link } from 'react-router-dom'
+import { PropTypes } from 'prop-types'
+
+class Home extends Component {
+
+    static propTypes = {
+        books: PropTypes.array.isRequired,
+        onChange: PropTypes.func.isRequired
+      }
+    
+    render() {
+        return (
+            <div className="list-books">
+            <div className="list-books-title">
+              <h1>MyReads</h1>
+            </div>
+            <div className="list-books-content">
+              <div>
+                <div className="bookshelf">
+                  <h2 className="bookshelf-title">Currently Reading</h2>
+                  <div className="bookshelf-books">
+                    <ol className="books-grid">
+                    {
+                          this.props.books
+                          .filter(book => book.shelf === 'currentlyReading')
+                          .map(book => (
+                            <li key={book.id}>
+                              <Book
+                                book={book}
+                                moveBooks={this.props.moveBooks}
+                                currentShelf="currentlyReading"
+                              />
+                            </li>
+                          ))
+                      }      
+                    </ol>
+                  </div>
+                </div>
+                <div className="bookshelf">
+                  <h2 className="bookshelf-title">Want to Read</h2>
+                  <div className="bookshelf-books">
+                    <ol className="books-grid">
+                      {
+                          this.props.books
+                          .filter(book => book.shelf === 'wantToRead')
+                          .map(book => (
+                            <li key={book.id}>
+                              <Book
+                                book={book}
+                                moveBooks={this.props.moveBooks}
+                                currentShelf="wantToRead"
+                              />
+                            </li>
+                          ))
+                      }      
+                    </ol>
+                  </div>
+                </div>
+                <div className="bookshelf">
+                  <h2 className="bookshelf-title">Read</h2>
+                  <div className="bookshelf-books">
+                    <ol className="books-grid">
+                    {
+                          this.props.books
+                          .filter(book => book.shelf === 'read')
+                          .map(book => (
+                            <li key={book.id}>
+                              <Book
+                                book={book}
+                                moveBooks={this.props.moveBooks}
+                                currentShelf="read"
+                              />
+                            </li>
+                          ))
+                      }      
+                    </ol>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="open-search">
+              <Link to="/search">Add a book</Link>
+            </div>
+          </div>
+        )
+    }
+}
+
+export default Home;
